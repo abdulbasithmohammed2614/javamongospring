1644211d284da32dfa16d13dd2bfe9b86c434529
localrepobasith <abdulbasithmohammed2614@gmail.com>
2023-09-16 14:45:38 +0530

    Fully functioning java project
    modified as per personal configs

--- src/test/java/SpringBoot/CRUDdemo/ApplicationTests.java
+++ src/test/java/SpringBoot/CRUDdemo/ApplicationTests.java
@@ -1,13 +1,13 @@
-package SpringBoot.CRUDdemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package SpringBoot.CRUDdemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
